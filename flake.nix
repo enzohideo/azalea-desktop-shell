62ceddaba16903c8a89950b70ea4b02b2ea7fee3
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-03-16 23:59:01 -0300

    initial commit

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    {
+      nixpkgs,
+      systems,
+      ...
+    }:
+    let
+      inherit (nixpkgs) lib;
+      forEachSystem = lib.genAttrs (import systems);
+      pkgsFor = forEachSystem (system: nixpkgs.legacyPackages.${system});
+      name = "lily-desktop-shell";
+    in
+    {
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = pkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            name = "lily-devshell";
+            RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+            buildInputs = with pkgs; [
+              cargo
+              rustc
+              rustfmt
+              pkg-config
+
+              gtk4
+              gtk4-layer-shell
+            ];
+          };
+        }
+      );
+    };
+}
