a098fd3771be459c185959b738c85724c3593d11
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-05-07 13:44:29 -0300

    azalea-service/trait: Add FromServices and IntoServices

--- azalea-service/src/trait.rs
+++ azalea-service/src/trait.rs
@@ -10,3 +10,20 @@ where
         None
     }
 }
+
+pub trait FromServices<Services>: Sized {
+    fn inherit(value: &Services) -> Self;
+}
+
+pub trait IntoServices<Services>: Sized {
+    fn strip(&self) -> Services;
+}
+
+impl<ChildServices, ParentServices> IntoServices<ChildServices> for ParentServices
+where
+    ChildServices: FromServices<ParentServices>,
+{
+    fn strip(&self) -> ChildServices {
+        ChildServices::inherit(self)
+    }
+}
