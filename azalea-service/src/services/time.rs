e4205d55ecc56e00455d307f87c5117e7ed84aac
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-05-11 23:59:53 -0300

    azalea-service: Handle Input::Stop to remotely stop Service

--- azalea-service/src/services/time.rs
+++ azalea-service/src/services/time.rs
@@ -1,29 +1,45 @@
+use std::sync::mpsc;
+
 use relm4::{ComponentSender, Worker};
+
+use crate::Input;
 
 pub type Output = chrono::DateTime<chrono::Local>;
 
 #[derive(Debug)]
 pub struct Model {
-    duration: std::time::Duration,
+    sender: mpsc::Sender<Input>,
 }
 
+// Add WorkerExt and impl Worker for WorkerExt
 impl Worker for Model {
     type Init = std::time::Duration;
-    type Input = crate::Input;
+    type Input = Input;
     type Output = Output;
 
-    fn init(init: Self::Init, _sender: ComponentSender<Self>) -> Self {
-        Self { duration: init }
+    fn init(init: Self::Init, sender: ComponentSender<Self>) -> Self {
+        let (tx, rx) = mpsc::channel();
+        let output = sender.output_sender().clone();
+
+        sender.oneshot_command(async move {
+            while let Ok(input) = rx.recv() {
+                match input {
+                    Input::Start => loop {
+                        if let Ok(Input::Stop) = rx.try_recv() {
+                            break;
+                        }
+                        let _ = output.send(chrono::Local::now());
+                        tokio::time::sleep(init).await;
+                    },
+                    Input::Stop => continue,
+                }
+            }
+        });
+
+        Self { sender: tx }
     }
 
-    fn update(&mut self, input: Self::Input, sender: ComponentSender<Self>) {
-        // TODO: Use a Command instead of blocking here
-        match input {
-            crate::Input::Start => loop {
-                let _ = sender.output(chrono::Local::now());
-                std::thread::sleep(self.duration);
-            },
-            crate::Input::Stop => todo!(),
-        }
+    fn update(&mut self, input: Self::Input, _sender: ComponentSender<Self>) {
+        drop(self.sender.send(input));
     }
 }
