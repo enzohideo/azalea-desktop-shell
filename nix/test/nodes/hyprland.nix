3ee490849d7355db983aed3890d017f3c5affb99
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-05-17 17:53:05 -0300

    nix/test: Set Hyprland config and remove uwsm

--- nix/test/nodes/hyprland.nix
+++ nix/test/nodes/hyprland.nix
@@ -1,13 +1,27 @@
-{ ... }:
+{ pkgs, ... }:
+let
+  cfg = pkgs.writeText "azalea-hyprland.conf" ''
+    exec-once = systemctl --user start azalea.service
+    exec-once = kitty
+
+    decoration {
+      blur {
+        enabled = false
+      }
+      active_opacity = 0.70
+      inactive_opacity = 0.70
+      fullscreen_opacity = 0.70
+    }
+  '';
+in
 {
-  programs.hyprland = {
-    enable = true;
-    withUWSM = true;
-  };
+  programs.hyprland.enable = true;
+
+  systemd.user.services.azalea.enable = true;
 
   programs.bash.loginShellInit = ''
-    if uwsm check may-start; then
-      exec uwsm start hyprland-uwsm.desktop
+    if [ "$(tty)" = "/dev/tty1" ]; then
+      exec dbus-run-session -- hyprland --config ${cfg}
     fi
   '';
 }
