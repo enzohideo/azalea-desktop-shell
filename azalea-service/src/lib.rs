e749764332ca7ade4d374806c9ea48e0ea7f505e
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-05-29 16:29:18 -0300

    azalea-service: Move service mod to lib.rs

--- azalea-service/src/lib.rs
+++ azalea-service/src/lib.rs
@@ -2,5 +2,39 @@ pub mod dbus;
 
 pub mod services;
 
-mod service;
-pub use service::*;
+mod handler;
+pub use handler::*;
+
+mod status;
+pub use status::*;
+
+pub mod error;
+
+use tokio::sync::broadcast;
+
+pub trait Service
+where
+    Self: Sized + Send + 'static,
+{
+    type Init: Clone + Send;
+    type Input: Clone + Send;
+    type Output: Clone + 'static + Send;
+
+    fn new(
+        init: Self::Init,
+        input_sender: broadcast::Sender<Self::Input>,
+        output_sender: broadcast::Sender<Self::Output>,
+    ) -> impl std::future::Future<Output = Self> + Send;
+    fn handler(init: Self::Init) -> Handler<Self> {
+        Handler::new(init)
+    }
+    fn message(
+        &mut self,
+        input: Self::Input,
+        output_sender: &broadcast::Sender<Self::Output>,
+    ) -> impl std::future::Future<Output = ()> + Send;
+    fn iteration(
+        &mut self,
+        output_sender: &broadcast::Sender<Self::Output>,
+    ) -> impl std::future::Future<Output = Result<(), error::Error>> + Send;
+}
