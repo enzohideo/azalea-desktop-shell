ffe47b85f973b03e913a26c00e6d91e2d99f8d4b
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-05-30 02:29:31 -0300

    azalea-window: taskbar/mpris: Keep track of player position

--- azalea-window/src/taskbar/widget/mpris.rs
+++ azalea-window/src/taskbar/widget/mpris.rs
@@ -1,15 +1,20 @@
 use azalea_service::{
-    LocalListenerHandle, StaticHandler, dbus::mpris::media_player2::PlaybackStatus, services,
+    LocalListenerHandle, StaticHandler,
+    dbus::mpris::media_player2::{PlaybackRate, PlaybackStatus},
+    services,
 };
-use gtk::prelude::BoxExt;
+use gtk::prelude::{BoxExt, OrientableExt};
 use relm4::{Component, ComponentParts, ComponentSender, component};
 
 // TODO: Stack factory
 crate::init! {
     Model {
         status: PlaybackStatus,
+        rate: PlaybackRate,
         title: Option<String>,
         artist: Option<String>,
+        length: Option<i64>,
+        position: f64,
         _event_listener_handle: LocalListenerHandle,
     }
 
@@ -22,12 +27,17 @@ pub enum Input {
     Event(services::dbus::mpris::Output),
 }
 
+#[derive(Debug)]
+pub enum CommandOutput {
+    PositionDelta(f64),
+}
+
 #[component(pub)]
 impl Component for Model {
     type Init = Init;
     type Input = Input;
     type Output = ();
-    type CommandOutput = ();
+    type CommandOutput = CommandOutput;
 
     view! {
         gtk::Revealer {
@@ -38,15 +48,32 @@ impl Component for Model {
 
             gtk::Box{
                 set_spacing: 12,
+                gtk::Box {
+                    set_orientation: gtk::Orientation::Vertical,
 
-                gtk::Label {
-                    #[watch]
-                    set_label: &model.title.to_owned().unwrap_or(format!("no title"))
-                },
+                    gtk::Label {
+                        #[watch]
+                        set_label: &model.title.to_owned().unwrap_or(format!("no title"))
+                    },
 
-                gtk::Label {
-                    #[watch]
-                    set_label: &model.artist.to_owned().unwrap_or(format!("no artist"))
+                    gtk::Box{
+                        set_spacing: 12,
+
+                        gtk::Label {
+                            #[watch]
+                            set_label: &model.artist.to_owned().unwrap_or(format!("no artist"))
+                        },
+
+                        gtk::Label {
+                            #[watch]
+                            set_label:
+                                &format!(
+                                    "{}/{}",
+                                    Self::format_time(model.position as i64),
+                                    model.length.map(Self::format_time).unwrap_or(format!("00:00"))
+                                )
+                        },
+                    },
                 },
             },
         }
@@ -59,13 +86,24 @@ impl Component for Model {
     ) -> ComponentParts<Self> {
         let model = Model {
             status: PlaybackStatus::Stopped,
+            rate: 1.,
             title: None,
             artist: None,
+            length: None,
+            position: 0.,
             _event_listener_handle: services::dbus::mpris::Service::forward_local(
                 sender.input_sender().clone(),
                 Input::Event,
             ),
         };
+
+        let cmd_sender = sender.command_sender().clone();
+        sender.oneshot_command(async move {
+            loop {
+                tokio::time::sleep(tokio::time::Duration::from_millis(1500)).await;
+                drop(cmd_sender.send(CommandOutput::PositionDelta(1500000.)));
+            }
+        });
 
         let widgets = view_output!();
 
@@ -84,9 +122,10 @@ impl Component for Model {
                                 .artist
                                 .map(|v| v.first().unwrap_or(&format!("no artist")).to_owned());
                             self.title = metadata.title;
+                            self.length = metadata.length;
                         }
-                        Event::Position(_) => {}
                         Event::PlaybackStatus(playback_status) => self.status = playback_status,
+                        Event::PlaybackRate(playback_rate) => self.rate = playback_rate,
                     };
                 }
             }
@@ -95,9 +134,31 @@ impl Component for Model {
 
     fn update_cmd(
         &mut self,
-        _message: Self::CommandOutput,
+        message: Self::CommandOutput,
         _sender: ComponentSender<Self>,
         _root: &Self::Root,
     ) {
+        match message {
+            CommandOutput::PositionDelta(delta) => {
+                if let PlaybackStatus::Playing = self.status {
+                    self.position += delta * self.rate
+                }
+            }
+        }
+    }
+}
+
+impl Model {
+    fn format_time(us: i64) -> String {
+        let time = us / 1000000;
+        let hours = time / 3600;
+        let minutes = time / 60 - hours * 60;
+        let seconds = time - minutes * 60 - hours * 3600;
+
+        if hours == 0 {
+            format!("{:0>2}:{:0>2}", minutes, seconds)
+        } else {
+            format!("{:0>2}:{:0>2}:{:0>2}", hours, minutes, seconds)
+        }
     }
 }
