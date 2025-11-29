1a1b88b43db6288f2e7eb3527598c52f0b0430c7
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-11-29 04:17:41 -0300

    azalea-shell: window/taskbar: Move register_widgets up a mod

--- azalea-shell/src/window/mod.rs
+++ azalea-shell/src/window/mod.rs
@@ -50,3 +50,38 @@ macro_rules! init {
         pub type Init = $crate::window::Init<Model>;
     };
 }
+
+#[macro_export]
+macro_rules! register_widgets {
+    ($($window:ident, $model:ty;)+) => {
+        use relm4::component::Connector;
+        use relm4::prelude::Component;
+
+        #[allow(dead_code)]
+        pub enum WidgetWrapper {
+            $($window(Connector<$model>)),+
+        }
+
+        #[allow(dead_code)]
+        #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+        pub enum ConfigWrapper {
+            $($window(<$model as crate::window::ModelExt>::Config)),+
+        }
+
+        #[allow(dead_code)]
+        impl ConfigWrapper {
+            pub fn build_widget(self) -> (WidgetWrapper, gtk::Widget) {
+                match self {
+                    $(ConfigWrapper::$window(config) => {
+                        let builder = <$model>::builder();
+                        let widget = builder.root.clone();
+                        let wrapper = WidgetWrapper::$window(
+                            builder.launch($crate::window::Init::new(config))
+                        );
+                        (wrapper, gtk::glib::object::Cast::upcast::<gtk::Widget>(widget))
+                    },)+
+                }
+            }
+        }
+    };
+}
--- azalea-shell/src/window/taskbar/widget/mod.rs
+++ azalea-shell/src/window/taskbar/widget/mod.rs
@@ -1,42 +1,10 @@
+use crate::register_widgets;
+
 pub mod bluetooth;
 pub mod media;
 pub mod network;
 pub mod search;
 pub mod time;
-
-macro_rules! register_widgets {
-    ($($window:ident, $model:ty;)+) => {
-        use relm4::component::Connector;
-        use relm4::prelude::Component;
-
-        #[allow(dead_code)]
-        pub enum WidgetWrapper {
-            $($window(Connector<$model>)),+
-        }
-
-        #[allow(dead_code)]
-        #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
-        pub enum ConfigWrapper {
-            $($window(<$model as crate::window::ModelExt>::Config)),+
-        }
-
-        #[allow(dead_code)]
-        impl ConfigWrapper {
-            pub fn build_widget(self) -> (WidgetWrapper, gtk::Widget) {
-                match self {
-                    $(ConfigWrapper::$window(config) => {
-                        let builder = <$model>::builder();
-                        let widget = builder.root.clone();
-                        let wrapper = WidgetWrapper::$window(
-                            builder.launch($crate::window::Init::new(config))
-                        );
-                        (wrapper, gtk::glib::object::Cast::upcast::<gtk::Widget>(widget))
-                    },)+
-                }
-            }
-        }
-    };
-}
 
 register_widgets!(
     Bluetooth, bluetooth::Model;
