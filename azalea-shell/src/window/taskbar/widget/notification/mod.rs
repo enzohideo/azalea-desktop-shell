64a4a9f80da19dc85a5db8d7ef05a808d4d9fc08
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-11-30 18:25:49 -0300

    azalea-shell: window/taskbar/notification: Show latest notification

--- azalea-shell/src/window/taskbar/widget/notification/mod.rs
+++ azalea-shell/src/window/taskbar/widget/notification/mod.rs
@@ -3,12 +3,14 @@ use gtk::prelude::*;
 use relm4::{ComponentParts, ComponentSender, SimpleComponent, component, factory::FactoryHashMap};
 
 use crate::{
-    factory,
+    factory, icon,
     service::{self, dbus::notification},
 };
 
 crate::init! {
     Model {
+        num_unread: usize,
+        latest_notification: Option<notification::service::Notification>,
         notifications: FactoryHashMap<u32, factory::notification::Model>,
         _service_handle: LocalListenerHandle,
     }
@@ -19,6 +21,7 @@ crate::init! {
 
 #[derive(Debug)]
 pub enum Input {
+    ClearLatest,
     Close(u32),
     Notifications(notification::Output),
 }
@@ -32,18 +35,54 @@ impl SimpleComponent for Model {
     view! {
         gtk::MenuButton {
             #[wrap(Some)]
-            set_child= &gtk::Label {
-                set_label: "notifications",
+            set_child= &gtk::Box {
+                set_spacing: 8,
+                set_hexpand: true,
+
+                gtk::Revealer {
+                    set_transition_type: gtk::RevealerTransitionType::SlideLeft,
+                    set_transition_duration: 1000,
+
+                    #[watch]
+                    set_reveal_child: model.latest_notification.is_some(),
+
+                    gtk::Label {
+                        set_max_width_chars: 30,
+                        set_ellipsize: gtk::pango::EllipsizeMode::End,
+
+                        #[watch]
+                        set_label: if model.latest_notification.is_some() {
+                            &model.latest_notification.as_ref().unwrap().summary
+                        } else { "" }
+                    },
+                },
+
+                gtk::Image {
+                    set_icon_name: Some(icon::BELL),
+                },
+
+                gtk::Label {
+                    #[watch]
+                    set_visible: model.num_unread > 0,
+
+                    #[watch]
+                    set_label: &format!("{}", model.num_unread),
+                },
             },
 
             #[wrap(Some)]
             set_popover = &gtk::Popover {
                 set_position: gtk::PositionType::Right,
 
+                connect_notify: (Some("visible"), move |this, _| {
+                    if !this.get_visible() {
+                        drop(sender.input_sender().send(Input::ClearLatest));
+                    }
+                }),
+
                 #[local_ref]
                 notifications_widget -> gtk::Box {
-                    add_css_class: "azalea-surface",
-                    add_css_class: "azalea-semi-transparent",
+                    add_css_class: "azalea-transparent",
                     set_orientation: gtk::Orientation::Vertical,
                     set_spacing: 5,
                 }
@@ -57,6 +96,8 @@ impl SimpleComponent for Model {
         sender: ComponentSender<Self>,
     ) -> ComponentParts<Self> {
         let model = Model {
+            latest_notification: None,
+            num_unread: 0,
             _service_handle: service::dbus::notification::Service::forward_local(
                 sender.input_sender().clone(),
                 Input::Notifications,
@@ -76,12 +117,17 @@ impl SimpleComponent for Model {
 
     fn update(&mut self, message: Self::Input, _sender: ComponentSender<Self>) {
         match message {
+            Input::ClearLatest => {
+                self.latest_notification = None;
+            }
             Input::Close(id) => {
                 self.notifications.remove(&id);
                 // TODO: Call Close method in notification service
             }
             Input::Notifications(message) => match message {
                 notification::Output::Notification(notification) => {
+                    self.num_unread += 1;
+                    self.latest_notification = Some(notification.clone());
                     self.notifications.insert(notification.id, notification);
                 }
             },
