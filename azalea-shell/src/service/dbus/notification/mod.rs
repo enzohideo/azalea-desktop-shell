ddb730a68c0419244438e2919825d2dd36a33cc7
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-11-30 16:27:50 -0300

    azalea-shell: notification: Only create server if it doesn't exist already

--- azalea-shell/src/service/dbus/notification/mod.rs
+++ azalea-shell/src/service/dbus/notification/mod.rs
@@ -2,12 +2,13 @@ pub mod service;
 
 use std::collections::HashMap;
 
-use tokio::sync::broadcast;
+use azalea_service::StaticHandler;
+use tokio::sync::{broadcast, oneshot};
 
 #[derive(azalea_derive::StaticHandler)]
 pub struct Service {
     notifications: HashMap<u32, service::Notification>,
-    _conn: Option<zbus::Connection>,
+    conn: Option<zbus::Connection>,
     rx: flume::Receiver<service::Event>,
 }
 
@@ -36,27 +37,57 @@ impl azalea_service::Service for Service {
     type Event = Event;
     type Output = Output;
 
+    fn handler(init: Self::Init) -> azalea_service::Handler<Self> {
+        azalea_service::Handler::new(init, 16, 16)
+    }
+
     async fn new(
         _init: Self::Init,
         _input: flume::Sender<Self::Input>,
         _output_sender: broadcast::Sender<Self::Output>,
     ) -> Self {
         let (tx, rx) = flume::unbounded();
+        let (discovery_tx, discovery_rx) = oneshot::channel();
+        super::discovery::Service::send(super::discovery::Input::QueryObjects(discovery_tx));
+        let conn = match discovery_rx.await {
+            Ok(names) => {
+                let mut notification_server_found = false;
 
-        let notifications = service::Notifications::new(tx);
-        let conn = if let Ok(conn) = zbus::conn::Builder::session()
-            .and_then(|conn| conn.name("org.freedesktop.Notifications"))
-            .and_then(|conn| conn.serve_at("/org/freedesktop/Notifications", notifications))
-            .map(|conn| conn.build())
-        {
-            conn.await.ok()
-        } else {
-            None
+                for name in names {
+                    if name.contains("org.freedesktop.Notifications") {
+                        notification_server_found = true;
+                        break;
+                    }
+                }
+
+                if notification_server_found {
+                    None
+                } else {
+                    let notifications = service::Notifications::new(tx);
+
+                    if let Ok(conn) = zbus::conn::Builder::session()
+                        .and_then(|conn| conn.name("org.freedesktop.Notifications"))
+                        .and_then(|conn| {
+                            conn.serve_at("/org/freedesktop/Notifications", notifications)
+                        })
+                        .map(|conn| conn.build())
+                    {
+                        conn.await.ok()
+                    } else {
+                        azalea_log::warning!("There's already a notification server running!");
+                        None
+                    }
+                }
+            }
+            Err(e) => {
+                azalea_log::warning!("Failed to query for other notification servers: {e}");
+                None
+            }
         };
 
         Self {
             notifications: Default::default(),
-            _conn: conn,
+            conn,
             rx,
         }
     }
@@ -73,8 +104,13 @@ impl azalea_service::Service for Service {
 
     async fn event_generator(&mut self) -> Self::Event {
         loop {
-            if let Ok(event) = self.rx.recv_async().await {
-                return Event::Notifications(event);
+            if self.conn.is_none() {
+                // TODO: Try to recreate?
+                tokio::time::sleep(std::time::Duration::from_secs(100)).await;
+            } else {
+                if let Ok(event) = self.rx.recv_async().await {
+                    return Event::Notifications(event);
+                }
             }
         }
     }
