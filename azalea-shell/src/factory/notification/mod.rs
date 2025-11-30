3e9e8abba9f0411eb0a9946fdb2d9da693d1507d
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-11-30 17:30:39 -0300

    azalea-shell: factory/notification: Display notification image

--- azalea-shell/src/factory/notification/mod.rs
+++ azalea-shell/src/factory/notification/mod.rs
@@ -1,11 +1,12 @@
-use gtk::prelude::*;
+use gtk::{gdk, glib, prelude::*};
 use relm4::{FactorySender, prelude::*};
 
-use crate::service::dbus::notification;
+use crate::{component::image, service::dbus::notification};
 
-#[derive(Debug)]
 pub struct Model {
     notification: notification::service::Notification,
+    image: relm4::Controller<image::Model>,
+    // image: Option<gdk::Texture>,
 }
 
 #[derive(Debug)]
@@ -32,19 +33,24 @@ impl FactoryComponent for Model {
     view! {
         #[root]
         gtk::Box {
-            set_orientation: gtk::Orientation::Vertical,
+            append: self.image.widget(),
 
-            gtk::Label {
-                #[watch]
-                set_label: &self.notification.app_name,
-            },
+            gtk::Box {
+                set_orientation: gtk::Orientation::Vertical,
 
-            gtk::Label {
-                #[watch]
-                set_label: &self.notification.body,
+                gtk::Label {
+                    #[watch]
+                    set_label: &self.notification.summary,
+                },
+
+                gtk::Label {
+                    #[watch]
+                    set_label: &self.notification.body,
+                },
             },
 
             gtk::Button {
+                set_valign: gtk::Align::Center,
                 set_label: "X",
                 connect_clicked => Input::Close
             },
@@ -52,7 +58,48 @@ impl FactoryComponent for Model {
     }
 
     fn init_model(notification: Self::Init, _index: &u32, _sender: FactorySender<Self>) -> Self {
-        Self { notification }
+        let model = Self {
+            image: image::Model::builder()
+                .launch(image::Init {
+                    fallback: None,
+                    width: None,
+                    height: Some(30),
+                })
+                .detach(),
+            notification: notification.clone(),
+        };
+
+        match notification.image.clone() {
+            Some(image) => match image {
+                notification::service::Image::Data {
+                    width,
+                    height,
+                    rowstride,
+                    has_alpha,
+                    bits_per_sample,
+                    channels: _,
+                    data,
+                } => {
+                    let pixbuf = gdk::gdk_pixbuf::Pixbuf::from_bytes(
+                        &glib::Bytes::from_owned(data),
+                        gtk::gdk_pixbuf::Colorspace::Rgb,
+                        has_alpha,
+                        bits_per_sample,
+                        width,
+                        height,
+                        rowstride,
+                    );
+
+                    drop(model.image.sender().send(image::Input::LoadPixbuf(pixbuf)));
+                }
+                notification::service::Image::Path(path) => {
+                    drop(model.image.sender().send(image::Input::LoadImage(path)));
+                }
+            },
+            None => (),
+        };
+
+        model
     }
 
     fn update(&mut self, message: Self::Input, sender: FactorySender<Self>) {
--- azalea-shell/src/service/dbus/notification/service.rs
+++ azalea-shell/src/service/dbus/notification/service.rs
@@ -1,12 +1,27 @@
 use std::collections::HashMap;
 
 #[derive(Clone, Debug)]
+pub enum Image {
+    Data {
+        width: i32,
+        height: i32,
+        rowstride: i32,
+        has_alpha: bool,
+        bits_per_sample: i32,
+        channels: i32,
+        data: Vec<u8>,
+    },
+    Path(String),
+}
+
+#[derive(Clone, Debug)]
 pub struct Notification {
     pub id: u32,
     pub app_name: String,
     pub app_icon: String,
     pub summary: String,
     pub body: String,
+    pub image: Option<Image>,
 }
 
 pub enum Event {
@@ -54,7 +69,7 @@ impl Notifications {
         summary: String,
         body: String,
         _actions: Vec<String>,
-        _hints: HashMap<String, zbus::zvariant::Value<'_>>,
+        hints: HashMap<String, zbus::zvariant::Value<'_>>,
         _expire_timeout: i32,
     ) -> u32 {
         let id = if replaces_id != 0 {
@@ -62,6 +77,28 @@ impl Notifications {
         } else {
             self.last_id_used = self.last_id_used + 1;
             self.last_id_used
+        };
+
+        let image = if hints.contains_key("image-path") {
+            hints
+                .get("image-path")
+                .map(|image_path| Image::Path(image_path.to_string()))
+        } else {
+            hints
+                .get("image-data")
+                .and_then(|image_data| zbus::zvariant::Structure::try_from(image_data).ok())
+                .and_then(|structure| {
+                    <(i32, i32, i32, bool, i32, i32, Vec<u8>)>::try_from(structure).ok()
+                })
+                .map(|data| Image::Data {
+                    width: data.0,
+                    height: data.1,
+                    rowstride: data.2,
+                    has_alpha: data.3,
+                    bits_per_sample: data.4,
+                    channels: data.5,
+                    data: data.6,
+                })
         };
 
         azalea_log::debug!(
@@ -76,6 +113,7 @@ impl Notifications {
                 app_icon,
                 summary,
                 body,
+                image,
             }))
             .await
         {
