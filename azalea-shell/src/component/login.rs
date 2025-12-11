030433762de5df437b372e3b608cc60b73349571
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-12-11 14:19:49 -0300

    azalea-shell: component/login: Add icons

--- azalea-shell/src/component/login.rs
+++ azalea-shell/src/component/login.rs
@@ -1,6 +1,6 @@
-use crate::service;
+use crate::{icon, service};
 use azalea_service::LocalStaticHandler;
-use gtk::prelude::ButtonExt;
+use gtk::prelude::*;
 use relm4::{ComponentParts, ComponentSender, SimpleComponent, component};
 use std::fmt::Debug;
 
@@ -14,21 +14,88 @@ impl SimpleComponent for Model {
 
     view! {
         gtk::Box {
+            set_hexpand: true,
+            set_halign: gtk::Align::Fill,
+
             gtk::Button {
-                set_label: "Suspend",
+                set_hexpand: true,
+                set_halign: gtk::Align::Fill,
+
+                #[wrap(Some)]
+                set_child = &gtk::Box {
+                    set_halign: gtk::Align::Center,
+                    set_spacing: 6,
+
+                    gtk::Image {
+                        set_icon_name: Some(icon::SUSPEND),
+                    },
+
+                    gtk::Label {
+                        set_label: "Suspend",
+                    },
+                },
+
                 connect_clicked => Self::Input::Suspend
             },
             gtk::Button {
-                set_label: "Hibernate",
-                connect_clicked => Self::Input::Hibernate
-            },
-            gtk::Button {
-                set_label: "Reboot",
+                set_hexpand: true,
+                set_halign: gtk::Align::Fill,
+
+                #[wrap(Some)]
+                set_child = &gtk::Box {
+                    set_halign: gtk::Align::Center,
+                    set_spacing: 6,
+
+                    gtk::Image {
+                        set_icon_name: Some(icon::REBOOT),
+                    },
+
+                    gtk::Label {
+                        set_label: "Reboot",
+                    },
+                },
+
                 connect_clicked => Self::Input::Reboot
             },
             gtk::Button {
-                set_label: "Shutdown",
+                set_hexpand: true,
+                set_halign: gtk::Align::Fill,
+
+                #[wrap(Some)]
+                set_child = &gtk::Box {
+                    set_halign: gtk::Align::Center,
+                    set_spacing: 6,
+
+                    gtk::Image {
+                        set_icon_name: Some(icon::SHUTDOWN),
+                    },
+
+                    gtk::Label {
+                        set_label: "Shutdown",
+                    },
+                },
+
                 connect_clicked => Self::Input::PowerOff
+            },
+            gtk::Button {
+                set_hexpand: true,
+                set_halign: gtk::Align::Fill,
+
+                #[wrap(Some)]
+                set_child = &gtk::Box {
+                    set_halign: gtk::Align::Center,
+                    set_spacing: 6,
+
+                    gtk::Image {
+                        set_icon_name: Some(icon::HIBERNATE),
+                    },
+
+                    gtk::Label {
+                        set_label: "Hibernate",
+                    },
+                },
+
+                connect_clicked => Self::Input::Hibernate
             },
         }
     }
